9f73b3a7b4c6b77c5625600222926d82fe1f3ff1
Thierry CAROLE <thcarole1@hotmail.com>
2024-07-07 16:55:47 +0200

    Update Notebooks

--- hr_data_analytics_package_folder/api_functions/plot_api.py
+++ hr_data_analytics_package_folder/api_functions/plot_api.py
@@ -241,3 +241,107 @@ def plot_step_count():
     plt.savefig('raw_data/step_count.png')
     plt.close()
     return 'raw_data/step_count.png'
+
+
+def plot_violin(y : str):
+    y_norm = y
+    if y in ['Sensor_Heartbeat(Average/Min)',
+             'Sensor_Proximity(1-highest/10-lowest)']:
+        if y == 'Sensor_Heartbeat(Average/Min)':
+            y_norm = 'Sensor_Heartbeat'
+        elif y == 'Sensor_Proximity(1-highest/10-lowest)':
+            y_norm = 'Sensor_Proximity'
+
+    #Delete previous any previous png file
+    file_path = f'raw_data/violin_{y_norm}.png'
+
+    file_exists = exists(file_path)
+    if file_exists:
+        os.remove(file_path)
+        print(f"File '{file_path}' deleted successfully.")
+
+    # Other inputs
+    x='left_Company'
+    hue='left_Company'
+
+    #Retrieve dataset from local directory
+    data = pd.read_csv(DATA_HR)
+
+    plt.figure(figsize=(10, 6))
+    sns.violinplot(x = x,
+                   y = y,
+                   hue = hue,
+                   data=data,
+                   palette={0: 'green', 1: 'red'},
+                   dodge=False,
+                   legend=False)
+    plt.title(f'Distribution of {y_norm} by Employee Status')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.xticks([0, 1], ['Stayed', 'Left'])
+    plt.legend([],[], frameon=False)
+    plt.tight_layout()
+    plt.savefig(f'raw_data/violin_{y_norm}.png')
+    plt.close()
+    return f'raw_data/violin_{y_norm}.png'
+
+
+def cross_tab_count_feat(x:str):
+    y = 'left_Company'
+
+    #Delete previous any previous png file
+    file_path = f'raw_data/crosstab_count_feat_{x}.png'
+
+    file_exists = exists(file_path)
+    if file_exists:
+        os.remove(file_path)
+        print(f"File '{file_path}' deleted successfully.")
+
+
+    #Retrieve dataset from local directory
+    data = pd.read_csv(DATA_HR)
+
+    labels = ['No', 'Yes']
+    gender_ct = pd.crosstab(data[x], data[y])
+    gender_ct.plot(kind='bar')
+    plt.title(f'{x} Distribution')
+    plt.xlabel(x)
+    plt.ylabel('Count')
+    plt.legend(title='Left Company', labels=labels)
+    plt.tight_layout()
+    plt.savefig(f'raw_data/crosstab_count_feat_{x}.png')
+    plt.close()
+    return f'raw_data/crosstab_count_feat_{x}.png'
+
+
+def freq_feat(input : str):
+    #Delete previous any previous png file
+    file_path = f'raw_data/freq_feat_{input}.png'
+
+    file_exists = exists(file_path)
+    if file_exists:
+        os.remove(file_path)
+        print(f"File '{file_path}' deleted successfully.")
+
+    #Retrieve dataset from local directory
+    data = pd.read_csv(DATA_HR)
+
+    n_bins=8
+    ax = (data
+          .loc[lambda x: x["left_Company"] == 0]
+          [input]
+          .plot
+          .hist(label="Did Not Leave Company",density=True,bins=n_bins,alpha=0.3)
+         )
+
+    (data
+     .loc[lambda x: x["left_Company"] == 1]
+     [input]
+     .plot
+     .hist(ax=ax, label="Left Company",density=True,bins=n_bins,alpha=0.3)
+    )
+    plt.xlabel(input)
+    plt.legend()
+    plt.savefig(f'raw_data/freq_feat_{input}.png')
+    plt.close()
+    return f'raw_data/freq_feat_{input}.png'
--- hr_data_analytics_package_folder/plots/plot_func.py
+++ hr_data_analytics_package_folder/plots/plot_func.py
@@ -192,3 +192,72 @@ def plot_step_count():
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig('raw_data/step_count.png')
+
+def plot_violin(x:str, y : str):
+    # x='left_Company'
+    hue='left_Company'
+
+    #Retrieve dataset from local directory
+    data = pd.read_csv(DATA_HR)
+
+    y_norm = y
+    if y in ['Sensor_Heartbeat(Average/Min)',
+             'Sensor_Proximity(1-highest/10-lowest)']:
+        if y == 'Sensor_Heartbeat(Average/Min)':
+            y_norm = 'Sensor_Heartbeat'
+        elif y == 'Sensor_Proximity(1-highest/10-lowest)':
+            y_norm = 'Sensor_Proximity'
+
+    plt.figure(figsize=(10, 6))
+    sns.violinplot(x = x,
+                   y = y,
+                   hue = hue,
+                   data=data,
+                   palette={0: 'green', 1: 'red'},
+                   dodge=False,
+                   legend=False)
+    plt.title(f'Distribution of {y_norm} by Employee Status')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.xticks([0, 1], ['Stayed', 'Left'])
+    plt.legend([],[], frameon=False)
+    plt.tight_layout()
+    plt.savefig(f'raw_data/violin_{y_norm}.png')
+
+def cross_tab_count_feat(x:str):
+    y = 'left_Company'
+    #Retrieve dataset from local directory
+    data = pd.read_csv(DATA_HR)
+
+    labels = ['No', 'Yes']
+    gender_ct = pd.crosstab(data[x], data[y])
+    gender_ct.plot(kind='bar')
+    plt.title(f'{x} Distribution')
+    plt.xlabel(x)
+    plt.ylabel('Count')
+    plt.legend(title='Left Company', labels=labels)
+    plt.tight_layout()
+    plt.savefig(f'raw_data/crosstab_count_feat_{x}.png')
+
+
+def freq_feat(input : str):
+    #Retrieve dataset from local directory
+    data = pd.read_csv(DATA_HR)
+
+    n_bins=8
+    ax = (data
+          .loc[lambda x: x["left_Company"] == 0]
+          [input]
+          .plot
+          .hist(label="Did Not Leave Company",density=True,bins=n_bins,alpha=0.3)
+         )
+
+    (data
+     .loc[lambda x: x["left_Company"] == 1]
+     [input]
+     .plot
+     .hist(ax=ax, label="Left Company",density=True,bins=n_bins,alpha=0.3)
+    )
+    plt.xlabel(input)
+    plt.legend()
+    plt.savefig(f'raw_data/freq_feat_{input}.png')
